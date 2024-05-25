3209fe87663f11354067658897ffea886b51f87b
dljimenez2324 <dljimenez2324@gmail.com>
2024-05-25 11:12:09 -0700

    react forms Examp import commented out to use only FormValidation with Zod only

--- react_formsl/src/App.tsx
+++ react_formsl/src/App.tsx
@@ -1,7 +1,7 @@
 // import FormUseRef from "./components/Forms/FormUseRef"
 // import FormUseState from "./components/Forms/FormUseState"
 import FormValidationZod from "./components/Forms/FormValidationZod"
-import ReactFormsExamp from "./components/Forms/ReactFormsExamp"
+// import ReactFormsExamp from "./components/Forms/ReactFormsExamp"
 // import UseStateVsUseRef from "./components/UseStateVsUseRef"
 
 
